1929acde4f3c9ddb189683a9863334a85620fcf0
Hari Raja Prabhu P <109938413+hariprabhu571@users.noreply.github.com>
2024-05-27 10:36:25 +0530

    Update program7.java

--- Assignments/01-first-java/program7.java
+++ Assignments/01-first-java/program7.java
@@ -1,2 +1,14 @@
+// To calculate Fibonacci Series up to n numbers.
 public class program7 {
+    public static void main(String args[]){
+        int n =10;
+        int a1 =0,a2=1;
+        System.out.print(a1+" "+a2+" ");
+        for(int i=2;i<n;i++){
+            int a3 = a1+a2;
+            a1=a2;
+            a2=a3;
+            System.out.print(a3+" ");
+        }
+    }
 }
